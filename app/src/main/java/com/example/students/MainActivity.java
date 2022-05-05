63b96ca0f29d1be5df2ccbf035c3b119fc9f8fff
Christina-Kontogiorgi <103931174+Christina-Kontogiorgi@users.noreply.github.com>
2022-05-05 13:00:56 +0300

    class Student added in MainActivity.java

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.students;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.students.databinding.ActivityMainBinding;
+
+class Student{
+    String name;
+    String surname;
+    int am;
+    int years;
+    double mo;
+    public Student(){
+        String all;
+        all=name+""+surname+""+am+""+years+""+mo;
+        Log.e("",all);
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binder= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binder.getRoot());
+
+        binder.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
     }
 }
